556df0c9c644db1f98ab53a6c38bb1ca700970fb
kianamcc <42532725+kianamcc@users.noreply.github.com>
2024-01-23 17:17:51 -0800

    removing unused variables

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,14 +1,5 @@
 import React from "react";
 import "./About.css";
-import {
-  SiJavascript,
-  SiHtml5,
-  SiCss3,
-  SiReact,
-  SiPostgresql,
-  SiExpress,
-} from "react-icons/si";
-import { FaNodeJs, FaGitAlt } from "react-icons/fa";
 
 const About = () => {
   return (
